8a29e304faf654120cb78e23a9d5b1d14c69a662
eduwr <eduwric@gmail.com>
2022-07-21 01:42:34 -0300

    open method

--- dynamic-connectivity/src/Percolation.ts
+++ dynamic-connectivity/src/Percolation.ts
@@ -25,14 +25,31 @@ abstract class AbstractPercolation {
 
 export class Percolation implements AbstractPercolation {
   uf: UF;
+  n: number;
 
   constructor(n: number) {
-    this.uf = new UF(n ** 2 + 2);
+    this.n = n;
+    this.uf = new UF(this.n ** 2 + 2);
 
     for (let i = 1; i <= n; i++) {
       this.uf.union(0, i);
       this.uf.union(this.uf.N - 1, this.uf.N - 1 - i);
     }
+  }
+
+  private getIndex(row: number, col: number) {
+    if (row <= 0 || col <= 0) return undefined;
+    return this.n * (row - 1) + col;
+  }
+
+  private getNeighbors(row: number, col: number) {
+    const middle = this.getIndex(row, col);
+    const top = this.getIndex(row - 1, col);
+    const right = this.getIndex(row, col + 1);
+    const bottom = this.getIndex(row + 1, col);
+    const left = this.getIndex(row, col - 1);
+
+    return [middle, top, right, bottom, left].filter(Boolean) as number[];
   }
 
   private withinRange(...numbers: number[]) {
@@ -47,13 +64,29 @@ export class Percolation implements AbstractPercolation {
         `inputs [${row} and ${col}] must be within 1 and ${this.uf.N - 2}`
       );
     }
+
+    const [center, ...neighbors] = this.getNeighbors(row, col);
+
+    neighbors.forEach((neighbor) => this.uf.union(center, neighbor));
   }
 
   public isOpen(row: number, col: number): boolean {
+    if (!this.withinRange(row, col)) {
+      throw new IllegalArgumentException(
+        `inputs [${row} and ${col}] must be within 1 and ${this.uf.N - 2}`
+      );
+    }
+
     return true;
   }
 
   public isFull(row: number, col: number): boolean {
+    if (!this.withinRange(row, col)) {
+      throw new IllegalArgumentException(
+        `inputs [${row} and ${col}] must be within 1 and ${this.uf.N - 2}`
+      );
+    }
+
     return true;
   }
 
@@ -69,18 +102,47 @@ export class Percolation implements AbstractPercolation {
     const percolation = new Percolation(4);
 
     console.log(percolation.uf.components);
-    console.log(percolation.uf.connected(0, 1));
-    console.log(percolation.uf.connected(0, 2));
-    console.log(percolation.uf.connected(0, 3));
-    console.log(percolation.uf.connected(0, 4));
+
+    const hasTopConnected = Array.from(
+      { length: percolation.n },
+      (_, k) => k + 1
+    )
+      .map((val) => percolation.uf.connected(0, val))
+      .every((val) => val === true);
+
+    console.log("Has top connected? ", hasTopConnected);
+
+    const hasBottomConnected = Array.from(
+      { length: percolation.n },
+      (_, k) => percolation.n ** 2 - k
+    )
+      .map((val) => percolation.uf.connected(percolation.uf.N - 1, val))
+      .every((val) => val === true);
+
+    console.log("Has Bottom connected? ", hasBottomConnected);
+
+    const areSecondAndBeforeLastRowDisconnected = [
+      [0, percolation.n + 1],
+      [percolation.uf.N - 1, percolation.n ** 2 - percolation.n],
+    ]
+      .map(([first, second]) => percolation.uf.connected(first, second))
+      .every((val) => val === false);
+
+    console.log(
+      "Are Second And Before Last Row Disconnected? ",
+      areSecondAndBeforeLastRowDisconnected
+    );
+
     console.log(percolation.uf.connected(0, 5));
-    console.log(percolation.uf.connected(17, 16));
-    console.log(percolation.uf.connected(17, 15));
-    console.log(percolation.uf.connected(17, 14));
-    console.log(percolation.uf.connected(17, 13));
     console.log(percolation.uf.N);
     console.log("Percolates?", percolation.percolates());
     console.log(percolation.uf.components.length);
+
+    percolation.open(2, 2);
+
+    console.log(percolation.uf.connected(0, 5));
+
+    console.log(percolation.uf.components);
   }
 }
 
